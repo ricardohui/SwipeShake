e9654fb6905205fe355d96e3dda7ec0248133b13
Ricardo Hui <eehaha@gmail.com>
2019-03-29 07:38:51 +0800

    Project completed

--- SwipeShake/ViewController.swift
+++ SwipeShake/ViewController.swift
@@ -13,8 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
     }
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEvent.EventSubtype.motionShake{
+            print("Device was shaken")
+        }
+    }
+    @objc func swiped(gesture: UIGestureRecognizer){
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            switch(swipeGesture.direction){
+                case UISwipeGestureRecognizer.Direction.right:
+                    print("User Swiped Right")
+                case UISwipeGestureRecognizer.Direction.left:
+                    print("User Swiped Left")
+                default:
+                    break
+            }
+        }
+    }
 
 }
 
